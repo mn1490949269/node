32699bea8344c32a541378106fc4694e478b89a4
mn <1490949269@qq.com>
2019-08-20 19:38:58 +0800

    提交

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
 
+/*
+function printMultiplyTable(firstNumber,secondNumber) {
+    var pro = new Array();
+    if(firstNumber>secondNumber){
+        return null;
+    }else if(firstNumber>=1 && firstNumber<=1000 && secondNumber>=1 && secondNumber<=1000){
+        pro=getProduct(firstNumber,secondNumber);
+        printTable(firstNumber,secondNumber,pro);
+        
+    }
+}
+function getProduct(firstNumber,secondNumber){
+    for(var i=firstNumber; i<=secondNumber; i++){
+        for(var j=firstNumber; j<=i; j++){
+            pro[i][j] = i+"*"+j+"="+i*j; 
+        }
+    }
+    return pro;
+}
+function printTable(firstNumber,secondNumber,pro){
+    for(var i=firstNumber; i<=secondNumber; i++){
+        for(var j=firstNumber; j<=i; j++){
+            if(i!=j){
+                document.write(pro[i][j]+" ");
+            }else{
+                document.write(pro[i][j]+"<br/>");
+            }
+        }
+    }
+
+}
+*/
+
 function multiplyTable(left, right) {
     var output = "";
    var valid = isValid(left,right);
