c3484be8d75f45c667d75dfac1baa9742a6ce9f1
mn <1490949269@qq.com>
2019-08-20 16:31:23 +0800

    10

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,27 @@ const add = require('../main');
 it ('should add two numbers', () => {
     expect(add(2, 3)).toBe(5);
 });
+
+it ('should multiply two numbers', () => {
+    //given
+    const firstNumber=1;
+    const secondNumber=2;
+
+    //when
+    const result =  multiply(firstNumber,secondNumber);
+
+    //then
+    expect(result.toBe("1*1=1,1*2=2,2*2=4"));
+});
+
+it ('should multiply two numbers', () => {
+    //given
+    const firstNumber=a;
+    const secondNumber=b;
+
+    //when
+    const result =  multiply(firstNumber,secondNumber);
+
+    //then
+    expect(result.toBe("请输入正确参数"));
+});
